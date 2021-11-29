02c26431dd5f549d3a8b08c85a4c1999858fc8b6
Le4o <devleonardoandrade@gmail.com>
2021-11-29 18:23:12 -0300

    Removing types/next

--- pages/api/url.ts
+++ pages/api/url.ts
@@ -1,9 +1,8 @@
-import { NextApiRequest } from "next";
-import { NextApiResponseServerIO } from "../../types/next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { exec } from 'child_process';
 import { parseJson, findExploits } from '../../utils/jsonParser';
 
-const urlRequest = async (req: NextApiRequest, res: NextApiResponseServerIO) => {
+const urlRequest = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const message = req.body;
     const nmapCommand = `nmap -sV --script vulners ${message.url} ${message.port ? message.port : ''} -oX public/out/${message.user}.xml`;
